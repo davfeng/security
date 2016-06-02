41ce7ebb11285c62f8d6e173aee268bb44c7c348
Dave Feng <fyonghong99@hotmail.com>
2016-06-02 14:29:02 +0800

    add AccessMaster project
    Signed-off-by: Dave Feng <fyonghong99@hotmail.com>

---
+++ AccessMaster/AccessMaster/Resource.h
@@ -0,0 +1,28 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by AccessMaster.rc
+//
+#define IDD_DIALOG                      101
+#define IDI_ACCESSMASTER                102
+#define IDC_COMBO1                      1000
+#define IDC_TYPE                        1000
+#define IDR_NAME                        1001
+#define IDR_HANDLE                      1002
+#define IDE_NAME                        1003
+#define IDE_PROCESSID                   1004
+#define IDE_PID                         1004
+#define IDE_HANDLE                      1005
+#define IDB_EDIT                        1006
+#define IDE_USAGE                       1007
+#define IDC_BINARY                      1008
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1009
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
